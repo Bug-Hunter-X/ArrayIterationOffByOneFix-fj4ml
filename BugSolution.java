60d0fae14fac02acbe9f741b11731710ebcf9eb5
BugHunter-X <askzenai@gmail.com>
2024-12-04 18:48:06 +0100

    Add BugSolution.java file

---
+++ BugSolution.java
@@ -0,0 +1,10 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        for (int i = 0; i < arr.length; i++) { //Corrected condition
+            arr[i] = i * 2; 
+        }
+        System.out.println(arr[0]); // this will print 0
+        System.out.println(arr[4]); // this will print 8
+    }
+}
